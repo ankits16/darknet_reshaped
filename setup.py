99f2eda5ce8b8a085918f860b5f7aa9f9939b4b1
Ankit Sachan <>
2021-12-15 22:19:19 +0530

    no message

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setup(
  version='1.0',
  description='Python Distribution Utilities',
  # packages=['darknet_src', 'darknet_src/ai_darknet_reshaped'],
- packages= find_packages(),
+ packages=find_packages(where='darknet_src'),
  # packages= find_packages(where='darknet_src/ai_darknet_reshaped'),
                     # find_packages ['ai_darknet_reshaped'],
  has_ext_modules=lambda: True,
